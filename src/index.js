bf9cabcc346fa8956bc568a8732c22bd6677ee02
MoisesD2001 <moiss.donoso@uc.cl>
2024-06-24 00:06:29 -0400

    a

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3dpydVDlb57LCajh4fZURNpTfLcRGmGw",
+  authDomain: "monitoreo-d-2024-2.firebaseapp.com",
+  databaseURL: "https://monitoreo-d-2024-2-default-rtdb.firebaseio.com",
+  projectId: "monitoreo-d-2024-2",
+  storageBucket: "monitoreo-d-2024-2.appspot.com",
+  messagingSenderId: "395288746126",
+  appId: "1:395288746126:web:8834ddd36b8fea9f6911df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const A = ref(db, "variables/a")
+
+onValue(A, (sanpshot) => {
+    console.log(sanpshot.val());
+    const out2 = document.getElementById("output1");
+    var num_a = sanpshot.val(); 
+    out1.innerHTML = num_a; 
+  });  
